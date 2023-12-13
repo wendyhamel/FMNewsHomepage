6b7d1637b8ecfd31e0162877306de83a76b9527b
wendyhamel <whamel@it4web.nl>
2023-12-13 18:59:58 +0100

    Styling mobile layout and menu

---
+++ src/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter', 'san-serif']
+			},
+			colors: {
+				'orange': 'hsl(35, 77%, 62%)',
+				'red': 'hsl(5, 85%, 63%)',
+				'white': 'hsl(36, 100%, 99%)',
+				'gray': 'hsl(233, 8%, 79%)',
+				'gray-blue': 'hsl(236, 13%, 42%)',
+				'dark-blue': 'hsl(240, 100%, 5%)',
+			},
+			fontSize: {
+				'4.5xl': ['2.5rem', { lineHeight: '2.5rem'}]
+			},
+		}
+	}
+}
